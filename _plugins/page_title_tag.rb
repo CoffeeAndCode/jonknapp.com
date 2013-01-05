0896f497dc2b9d07c6477ecb307b6be2d3e23b98
Jonathan Knapp <jon@coffeeandcode.com>
2013-01-05 15:01:41 -0500

    Changed the description of the site, created a page_title plugin for Jekyll to show the author name or "post title by author" on posts

---
+++ _plugins/page_title_tag.rb
@@ -0,0 +1,19 @@
+module Jekyll
+  class PageTitleTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+    end
+
+    def render(context)
+      current_page = context.environments.first['page']
+      author = current_page['author'] || context.registers[:site].config['defaults']['author']
+      if current_page['id']
+        "#{current_page['title']} by #{author}"
+      else
+        "#{author}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('page_title', Jekyll::PageTitleTag)
