fe1ac1faf823ec3833d406974236060736ed7bba
Jonathan Knapp <jon@coffeeandcode.com>
2013-01-06 16:17:19 -0500

    Added closure compiler, less precompilation, started cleaning up css

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source "http://rubygems.org"
 
+gem "closure-compiler"
 gem "jekyll"
 gem "jekyll-asset-pipeline"
+gem "less"
 gem "rdiscount"
+gem "therubyracer"
+gem "yui-compressor"
--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -11,13 +11,38 @@ module JekyllAssetPipeline
     end
   end
 
-  class JavascriptTagTemplate < JekyllAssetPipeline::Template
+  class JavaScriptCompressor < JekyllAssetPipeline::Compressor
+    require 'closure-compiler'
+
+    def self.filetype
+      '.js'
+    end
+
+    def compress
+      return Closure::Compiler.new.compile(@content)
+    end
+  end
+
+  class JavaScriptTagTemplate < JekyllAssetPipeline::Template
     def self.filetype
       '.js'
     end
 
     def html
       "<script src=\"/#{@path}/#{@filename}\" type=\"text/javascript\"></script>"
+    end
+  end
+
+  class LessConverter < JekyllAssetPipeline::Converter
+    require 'less'
+
+    def self.filetype
+      '.less'
+    end
+
+    def convert
+      parser = Less::Parser.new :paths => ['_assets/css/'], :filename => 'style.less'
+      return parser.parse(@content).to_css
     end
   end
 end
